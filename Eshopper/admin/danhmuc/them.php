324f8329ddafc7d88626e25adb4813c7ab776a6b
dungnnph <dungnnph20620@fpt.edu.vn>
2022-11-11 15:45:04 +0700

    project

---
+++ Eshopper/admin/danhmuc/them.php
@@ -0,0 +1,24 @@
+
+<div class="">
+            <div class="row adtitle">
+                <h1>Thêm mới loại hàng hóa</h1>
+            </div>
+            <div class="frm_content">
+                <form action="index.php?act=addcat" method="POST">
+                    <label for="" class="mb">Mã loại <br>
+                    <input type="text" name="maloai" id="" disabled placeholder="Auto Number"><br>
+                </label>
+                <label for="" class="mb">Tên loại <br>
+                    <input type="text" name="tenloai" id=""><br><br>
+                </label>
+                
+                <br><br>
+                <label for="">
+                    <a href=""><input type="submit" name="submit" id="" value="THÊM MỚI"></a>
+                    <a href=""><input type="reset" name="reset" id="" value="NHẬP LẠI"></a>
+                    <a href=""><input type="button" name="" id="" value="DANH SÁCH"></a>
+                </label>
+                </form>
+            </div>
+        </div>
+    </div>
---
+++ Eshopper/admin/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../admin/css/style.css">
+    <link rel="stylesheet" href="../admin/css/addmin.css">
+    <title>EShopper</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row mb header_admin mt">
+            <h1>Quản trị  EShopper</h1>
+        </div>
+        <div class="row mb menu">
+            <ul>
+                <li class="mr"><a href="">Trang chủ</a></li>
+                <li class="mr"><a href="">Danh mục</a></li>
+                <li class="mr"><a href="">Hàng hóa</a></li>
+                <li class="mr"><a href="">Khách hàng</a></li>
+                <li class="mr"><a href="">Bình luận</a></li>
+                <li class="mr"><a href="">Thống kê</a></li>
+                <li><a href="">Danh sách đơn hàng</a></li>
+            </ul>
+        </div>
